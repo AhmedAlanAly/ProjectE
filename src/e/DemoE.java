3eecb469607b7719c2b235ee0262bb36a8c8e79b
araslan <araslan@localhost>
2024-01-03 20:16:01 -0600

    Initial Commit

---
+++ src/e/DemoE.java
@@ -0,0 +1,16 @@
+package e;
+
+public class DemoE {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Friday");
+		
+		System.out.println("One more time");
+		System.out.println("We're doing great!");
+
+		
+	}
+
+
+}
